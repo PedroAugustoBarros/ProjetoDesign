59a9950a981d1bfea34b79a32af9656253c18cd6
PedroBarrosDev <pedroaugustobarros@gmail.com>
2018-01-23 16:23:25 -0300

    v.0.0.1

--- app/src/main/java/br/com/pedro/projetodesign/MainActivity.java
+++ app/src/main/java/br/com/pedro/projetodesign/MainActivity.java
@@ -1,8 +1,16 @@
 package br.com.pedro.projetodesign;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +20,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private static RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private static RecyclerView recyclerView;
+    private static ArrayList<DataModel> data;
+    static View.OnClickListener myOnClickListener;
+    private static ArrayList<Integer> removedItems;
+
+    FrameLayout frameLayoutFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +45,17 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+
+        frameLayoutFrag = (FrameLayout) findViewById(R.id.frameLayoutFragment);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +65,85 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+//------------------------------------------
+
+        myOnClickListener = new MyOnClickListener(this);
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        data = new ArrayList<DataModel>();
+        for (int i = 0; i < MyData.nameArray.length; i++) {
+            data.add(new DataModel(
+                    MyData.nameArray[i],
+                    MyData.versionArray[i],
+                    MyData.id_[i],
+                    MyData.drawableArray[i]
+            ));
+        }
+
+        removedItems = new ArrayList<Integer>();
+
+        adapter = new CustomAdapter(data);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    private class MyOnClickListener implements View.OnClickListener {
+
+        private final Context context;
+
+        private MyOnClickListener(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void onClick(View v) {
+//            removeItem(v);
+            FragmentManager mFragmentManager;
+            mFragmentManager = getSupportFragmentManager();
+            FragmentTransaction ft = mFragmentManager.beginTransaction();
+
+            Bundle bundle = new Bundle();
+            String myMessage = "Stackoverflow is cool!";
+            bundle.putString("message", myMessage );
+//
+            DetalhesFragment compFragment = (DetalhesFragment) getSupportFragmentManager().findFragmentByTag("validar");
+
+            if (compFragment == null) {
+                compFragment = new DetalhesFragment();
+            }
+
+            compFragment.setArguments(bundle);
+
+            ft.replace(R.id.frameLayoutFragment, compFragment, "detalhes");
+            ft.addToBackStack("detalhes");
+            frameLayoutFrag.setVisibility(View.VISIBLE);
+            ft.commit();
+        }
+
+//        private void removeItem(View v) {
+//            int selectedItemPosition = recyclerView.getChildPosition(v);
+//            RecyclerView.ViewHolder viewHolder
+//                    = recyclerView.findViewHolderForPosition(selectedItemPosition);
+//            TextView textViewName
+//                    = (TextView) viewHolder.itemView.findViewById(R.id.textViewName);
+//            String selectedName = (String) textViewName.getText();
+//            int selectedItemId = -1;
+//            for (int i = 0; i < MyData.nameArray.length; i++) {
+//                if (selectedName.equals(MyData.nameArray[i])) {
+//                    selectedItemId = MyData.id_[i];
+//                }
+//            }
+//            removedItems.add(selectedItemId);
+//            data.remove(selectedItemPosition);
+//            adapter.notifyItemRemoved(selectedItemPosition);
+//        }
     }
 
     @Override
@@ -48,8 +152,28 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+//            super.onBackPressed();
+            if (frameLayoutFrag.getVisibility() == View.VISIBLE) {
+                frameLayoutFrag.setVisibility(View.GONE);
+
+//                fragmentAbilitarVoltar(false);
+
+//                String nomebanca = UsuarioSingleton.getUsuario().getNome_banca();
+//                getSupportActionBar().setTitle(nomebanca);
+            } else {
+                new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert).setTitle("Sair")
+                        .setMessage("Tem certeza que deseja sair?")
+                        .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        }).setNegativeButton("Não", null).show();
+            }
         }
+
+
+
     }
 
     @Override
---
+++ app/src/main/java/br/com/pedro/projetodesign/MyData.java
@@ -0,0 +1,19 @@
+package br.com.pedro.projetodesign;
+
+
+
+public class MyData {
+
+    static String[] nameArray = {"Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread", "Honeycomb", "Ice Cream Sandwich","JellyBean", "Kitkat", "Lollipop", "Marshmallow"};
+    static String[] versionArray = {"1.5", "1.6", "2.0-2.1", "2.2-2.2.3", "2.3-2.3.7", "3.0-3.2.6", "4.0-4.0.4", "4.1-4.3.1", "4.4-4.4.4", "5.0-5.1.1","6.0-6.0.1"};
+
+    static Integer[] drawableArray = {R.drawable.ic_menu_camera, R.drawable.ic_menu_gallery, R.drawable.ic_menu_manage,
+            R.drawable.ic_menu_send, R.drawable.ic_menu_gallery, R.drawable.ic_menu_camera, R.drawable.side_nav_bar,
+            R.drawable.ic_menu_slideshow, R.drawable.ic_menu_slideshow, R.drawable.ic_menu_gallery,R.drawable.ic_menu_share};
+
+//    static Integer[] drawableArray = {R.drawable.cupcake, R.drawable.donut, R.drawable.eclair,
+//            R.drawable.froyo, R.drawable.gingerbread, R.drawable.honeycomb, R.drawable.ics,
+//            R.drawable.jellybean, R.drawable.kitkat, R.drawable.lollipop,R.drawable.marsh};
+
+    static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+}
